b15796bf315296703f038f3b00256d846b72ca5b
marelycarcamo <marely.carcamo@gmail.com>
2021-03-10 02:49:52 -0300

    funciona !!

--- js/menuDinamico.js
+++ js/menuDinamico.js
@@ -5,33 +5,43 @@ const appMenu ={
 			seccionA:["submenuA1", "submenuA2", "submenuA3"],
 			seccionB:["submenuB1", "submenuB2", "submenuB3"],
 			seccionC:["submenuC1", "submenuC2", "submenuC3"],
+			selected:"",
+			texto:"",
+			arreglo:[]
 			
 		}
-	}
-}
+	},
+	methods:{
+		change(){
+					
+			switch(Number(this.selected)){
 
-const appFormulario={
-	data(){
-		return{
-			selected:"",
-			texto:""
+				case 1:
+					this.arreglo = this.seccionA
+					break;
+				case 2:
+					this.arreglo = this.seccionB
+					break;
+				case 3:
+					this.arreglo = this.seccionC
+					break;
+			
+			}
+		},
+
+		click(){
+		
+		this.arreglo.push(this.texto);
+		console.log(this.arreglo);
+
 		}
+		
+
 	}
 }
 
 
 
-// $("#bt-agregar").click(function(){
-// 	let submenu=$("#tx-submenu").val();
-// 	 selectMenu = $("select").val();
-
-// });
-	
-
-	// $("select").change(function(){
-	// 	selectMenu = $("select").val();
-	// 	alert(selectMenu)
-	// });
 
 
 
